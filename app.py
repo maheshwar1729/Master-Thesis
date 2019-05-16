8b63fba1d90887575187ef797b0b0f7fdda71bea
maheshwar reddy chappidi <maheshwarreddy.c1729@gmail.com>
2019-05-16 17:06:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from urllib.parse import unquote
-
-
 app = Flask(__name__)
 CORS(app)
-
-
 @app.route("/")
 def log():
     with open("genes.csv", "a") as f:
