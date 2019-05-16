699ae2b03b09606c7726402881e1c170ae24e1a0
maheshwar reddy chappidi <maheshwarreddy.c1729@gmail.com>
2019-05-16 17:08:17 +0200

    Update coverage.py

--- coverage.py
+++ coverage.py
@@ -6,8 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.chrome.options import Options
 import os
-
-
 class PythonOrgSearch(unittest.TestCase):
     def setUp(self, src_path=""):
         myProxy = "localhost:3128"
@@ -24,17 +22,13 @@ class PythonOrgSearch(unittest.TestCase):
         self.driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=chrome_options)
         cmd = "java -jar ./JSCover-2.0.6/target/dist/JSCover.jar -ws --no-branch --document-root={} --report-dir=target --no-instrument=test --no-instrument=js/lib".format(src_path)
         os.system(cmd)
-
     def test_search_in_python_org(self):
         driver = self.driver
         driver.get("http://localhost-proxy:8080/index.html")
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "radio1")))
         driver.find_element_by_id('radio1').click()
         driver.execute_script("jscoverage_report('python');");
-
     def tearDown(self):
         self.driver.close()
-
-
 if __name__ == '__main__':
     unittest.main()
